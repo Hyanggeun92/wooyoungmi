7cc6fdeae75132db07c905a0d2d3c59889a0e3fd
unknown <nabee29@naver.com>
2023-09-15 12:39:22 +0900

    first react shoppingmall project

---
+++ src/component/ProductCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+
+const ProductCard = ({item}) => {
+    const dataPrice = (item.price)
+    const dataPrice2= dataPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    console.log(dataPrice2);
+  return (
+    <div className='product-list'>
+        <div className="product-img"><img src={item?.img} alt="" /></div>
+        <div className="choice">{item?.choice===true?'Conscious Choice':''}</div>
+        <div className="title">{item?.title}</div>
+        <div className="price">{dataPrice2}</div>
+        <div className="new">{item?.new===true?'[신상품]':''}</div>
+
+      
+    </div>
+  )
+}
+
+export default ProductCard
